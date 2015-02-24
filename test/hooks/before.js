14874ba52a609c8a614e2ba960d1e15eb40bb3a6
christian-bromann <contact@christian-bromann.com>
2015-02-24 18:11:00 +0100

    wording fix

--- test/hooks/before.js
+++ test/hooks/before.js
@@ -4,7 +4,7 @@ var WebdriverIO = require('webdriverio'),
 var BeforeHook = module.exports = function(done) {
 
     var options = config.options;
-    options.desiredCapabilites = config.capabilities;
+    options.desiredCapabilities = config.capabilities;
 
     this.browser = WebdriverIO.remote(options);
     this.browser.init().call(done);
