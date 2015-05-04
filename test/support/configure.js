aac59d0a8de8c0371b3229c78cc0e4565020632f
Jakub Zmudzinski <jakub.zmudzinski@allegro.pl>
2015-05-04 10:21:43 +0200

    Used extend instead of deepmerge

--- test/support/configure.js
+++ test/support/configure.js
@@ -4,18 +4,18 @@
  */
 var fs = require('fs'),
     path = require('path')
-    merge = require('deepmerge'),
+    extend = require('extend'),
     config = require('../config.js').config,
     envConfigPath = path.join(__dirname, '..', 'config.' + process.env.NODE_ENV + '.js');
 
 if(process.env.NODE_ENV && fs.existsSync(envConfigPath)) {
-    config = merge(config, require(envConfigPath).config);
+    config = extend(true, config, require(envConfigPath).config);
 }
 
 if(process.env.TESTS_CONFIGURATION_PATH) {
     var testsConfigPath = path.join(__dirname, '..', '..', '..', '..', process.env.TESTS_CONFIGURATION_PATH);
     if (fs.existsSync(testsConfigPath)) {
-        config = merge(config, require(testsConfigPath).config);
+        config = extend(true, config, require(testsConfigPath).config);
     }
 }
 
