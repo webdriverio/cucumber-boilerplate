4d6cb3c3136baf9b46283b7cff83eb8b90dabeed
W.van Kuipers <w.vankuipers@lukkien.com>
2015-11-13 17:28:17 +0100

    Fixed grammar issues in ruling results

--- test/support/helper/checkWithinViewport.js
+++ test/support/helper/checkWithinViewport.js
@@ -1,9 +1,9 @@
 module.exports = function (selector, falseCase, done) {
     this.browser.isVisibleWithinViewport(selector).then(function(isVisible) {
         if(falseCase) {
-            isVisible.should.not.equal(true, 'Expected element "' + selector + '" to be outside the viewport, but is not');
+            isVisible.should.not.equal(true, 'Expected element "' + selector + '" to be outside the viewport, but it is not');
         } else {
-            isVisible.should.equal(true, 'Expected element "' + selector + '" to be inside the viewport, but is not');
+            isVisible.should.equal(true, 'Expected element "' + selector + '" to be inside the viewport, but it is not');
         }
     }).call(done);
 };
