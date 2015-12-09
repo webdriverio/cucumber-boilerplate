866a4c155778dd226fc337191687922333c42b5f
W.van Kuipers <w.vankuipers@lukkien.com>
2015-12-09 08:03:28 +0100

    Added [then] ^I expect that the path is( not)* "$string"$ rule

--- test/steps/then.js
+++ test/steps/then.js
@@ -43,6 +43,9 @@ module.exports = function (dict) {
         .then(/^I expect that element "$string" is( not)* within the viewport$/,
             require('../support/check/checkWithinViewport'))
 
+        .then(/^I expect that the path is( not)* "$string"$/,
+            require('../support/check/checkURLPath'))
+
         .then(/^I expect the url to( not)* contain "$string"$/,
             require('../support/check/checkInURLPath'))
 
