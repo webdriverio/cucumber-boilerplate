b3c8d7876f0d54622808b093f009e982cdfbfbfb
W.van Kuipers <w.vankuipers@lukkien.com>
2016-09-06 19:17:31 +0200

    Changed location of local server to localhost

--- wdio.conf.js
+++ wdio.conf.js
@@ -68,7 +68,7 @@ exports.config = {
     //
     // Set a base URL in order to shorten url command calls. If your url parameter starts
     // with "/", then the base url gets prepended.
-    baseUrl: 'http://127.0.0.1:8080',
+    baseUrl: 'http://localhost:8080',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
