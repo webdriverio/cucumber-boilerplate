341ba547a428f6ffc9b8cc20d16914fcfb142a91
W. van Kuipers <w.vankuipers@lukkien.com>
2017-01-12 14:44:08 +0100

    Added checkIfElementExists lib and implemented it where needed

--- src/support/check/checkElementExists.js
+++ src/support/check/checkElementExists.js
@@ -1,3 +1,5 @@
+import checkIfElementExists from '../lib/checkIfElementExists';
+
 /**
  * Check if the given element exists
  * @param  {String}   isExisting Whether the element should be existing or not
@@ -6,24 +8,10 @@
  * @param  {Function} done       Function to execute when finished
  */
 module.exports = (isExisting, elem, done) => {
-    /**
-     * The number of elements found in the DOM
-     * @type {Int}
-     */
-    const nrOfElements = browser.elements(elem).value;
-
     if (isExisting === 'an') {
-        expect(nrOfElements).to.have.length
-            .above(
-                0,
-                `Element with selector "${elem}" should exist on the page`
-            );
+        checkIfElementExists(elem);
     } else {
-        expect(nrOfElements).to.have
-            .lengthOf(
-                0,
-                `Element with selector "${elem}" should not exist on the page`
-            );
+        checkIfElementExists(elem, true);
     }
 
     done();
