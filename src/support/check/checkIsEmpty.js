88a797b1ffdc145ea9969d1c6c5f7315aeb45ecd
W. van Kuipers <w.vankuipers@lukkien.com>
2017-07-12 21:28:23 +0200

    Removed console.log leftovers

--- src/support/check/checkIsEmpty.js
+++ src/support/check/checkIsEmpty.js
@@ -4,16 +4,12 @@ module.exports = (element, falseCase, done) => {
     let newFalseCase = true;
     let newDone = done;
 
-    console.log(element, falseCase);
-
     if (typeof falseCase === 'function') {
         newDone = falseCase;
         newFalseCase = false;
     } else if (falseCase === ' not') {
         newFalseCase = false;
     }
-
-    console.log(element, newFalseCase);
 
     return checkContainsAnyText(element, newFalseCase, newDone);
 };
