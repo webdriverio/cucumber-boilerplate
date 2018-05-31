d5f7b016bd9cbe1d8e1cdb55894e8377a32bdf6c
Ludovic Roche <lroche94@gmail.com>
2018-05-31 21:40:52 +0200

    defineSupportCode is deprecated. Require/import the individual methods instead

--- src/steps/given.js
+++ src/steps/given.js
@@ -1,4 +1,4 @@
-import { defineSupportCode } from 'cucumber';
+const { Given } = require('cucumber');
 
 import checkContainsAnyText from '../support/check/checkContainsAnyText';
 import checkIsEmpty from '../support/check/checkIsEmpty';
@@ -22,109 +22,107 @@ import openWebsite from '../support/action/openWebsite';
 import resizeScreenSize from '../support/action/resizeScreenSize';
 
 
-defineSupportCode(({ Given }) => {
-    Given(
-        /^I open the (url|site) "([^"]*)?"$/,
-        openWebsite
-    );
+Given(
+    /^I open the (url|site) "([^"]*)?"$/,
+    openWebsite
+);
 
-    Given(
-        /^the element "([^"]*)?" is( not)* visible$/,
-        isVisible
-    );
+Given(
+    /^the element "([^"]*)?" is( not)* visible$/,
+    isVisible
+);
 
-    Given(
-        /^the element "([^"]*)?" is( not)* enabled$/,
-        isEnabled
-    );
+Given(
+    /^the element "([^"]*)?" is( not)* enabled$/,
+    isEnabled
+);
 
-    Given(
-        /^the element "([^"]*)?" is( not)* selected$/,
-        checkSelected
-    );
+Given(
+    /^the element "([^"]*)?" is( not)* selected$/,
+    checkSelected
+);
 
-    Given(
-        /^the checkbox "([^"]*)?" is( not)* checked$/,
-        checkSelected
-    );
+Given(
+    /^the checkbox "([^"]*)?" is( not)* checked$/,
+    checkSelected
+);
 
-    Given(
-        /^there is (an|no) element "([^"]*)?" on the page$/,
-        checkElementExists
-    );
+Given(
+    /^there is (an|no) element "([^"]*)?" on the page$/,
+    checkElementExists
+);
 
-    Given(
-        /^the title is( not)* "([^"]*)?"$/,
-        checkTitle
-    );
+Given(
+    /^the title is( not)* "([^"]*)?"$/,
+    checkTitle
+);
 
-    Given(
-        /^the element "([^"]*)?" contains( not)* the same text as element "([^"]*)?"$/,
-        compareText
-    );
+Given(
+    /^the element "([^"]*)?" contains( not)* the same text as element "([^"]*)?"$/,
+    compareText
+);
 
-    Given(
-        /^the (button|element) "([^"]*)?"( not)* matches the text "([^"]*)?"$/,
-        checkEqualsText
-    );
+Given(
+    /^the (button|element) "([^"]*)?"( not)* matches the text "([^"]*)?"$/,
+    checkEqualsText
+);
 
-    Given(
-        /^the (button|element) "([^"]*)?"( not)* contains the text "([^"]*)?"$/,
-        checkContainsText
-    );
+Given(
+    /^the (button|element) "([^"]*)?"( not)* contains the text "([^"]*)?"$/,
+    checkContainsText
+);
 
-    Given(
-        /^the (button|element) "([^"]*)?"( not)* contains any text$/,
-        checkContainsAnyText
-    );
+Given(
+    /^the (button|element) "([^"]*)?"( not)* contains any text$/,
+    checkContainsAnyText
+);
 
-    Given(
-        /^the (button|element) "([^"]*)?" is( not)* empty$/,
-        checkIsEmpty
-    );
+Given(
+    /^the (button|element) "([^"]*)?" is( not)* empty$/,
+    checkIsEmpty
+);
 
-    Given(
-        /^the page url is( not)* "([^"]*)?"$/,
-        checkUrl
-    );
+Given(
+    /^the page url is( not)* "([^"]*)?"$/,
+    checkUrl
+);
 
-    Given(
-        /^the( css)* attribute "([^"]*)?" from element "([^"]*)?" is( not)* "([^"]*)?"$/,
-        checkProperty
-    );
+Given(
+    /^the( css)* attribute "([^"]*)?" from element "([^"]*)?" is( not)* "([^"]*)?"$/,
+    checkProperty
+);
 
-    Given(
-        /^the cookie "([^"]*)?" contains( not)* the value "([^"]*)?"$/,
-        checkCookieContent
-    );
+Given(
+    /^the cookie "([^"]*)?" contains( not)* the value "([^"]*)?"$/,
+    checkCookieContent
+);
 
-    Given(
-        /^the cookie "([^"]*)?" does( not)* exist$/,
-        checkCookieExists
-    );
+Given(
+    /^the cookie "([^"]*)?" does( not)* exist$/,
+    checkCookieExists
+);
 
-    Given(
-        /^the element "([^"]*)?" is( not)* ([\d]+)px (broad|tall)$/,
-        checkDimension
-    );
+Given(
+    /^the element "([^"]*)?" is( not)* ([\d]+)px (broad|tall)$/,
+    checkDimension
+);
 
-    Given(
-        /^the element "([^"]*)?" is( not)* positioned at ([\d]+)px on the (x|y) axis$/,
-        checkOffset
-    );
+Given(
+    /^the element "([^"]*)?" is( not)* positioned at ([\d]+)px on the (x|y) axis$/,
+    checkOffset
+);
 
-    Given(
-        /^I have a screen that is ([\d]+) by ([\d]+) pixels$/,
-        resizeScreenSize
-    );
+Given(
+    /^I have a screen that is ([\d]+) by ([\d]+) pixels$/,
+    resizeScreenSize
+);
 
-    Given(
-        /^I have closed all but the first (window|tab)$/,
-        closeAllButFirstTab
-    );
+Given(
+    /^I have closed all but the first (window|tab)$/,
+    closeAllButFirstTab
+);
 
-    Given(
-        /^a (alertbox|confirmbox|prompt) is( not)* opened$/,
-        checkModal
-    );
-});
+Given(
+    /^a (alertbox|confirmbox|prompt) is( not)* opened$/,
+    checkModal
+);
--- src/steps/when.js
+++ src/steps/when.js
@@ -1,4 +1,4 @@
-import { defineSupportCode } from 'cucumber';
+const { When } = require('cucumber');
 
 import clearInputField from '../support/action/clearInputField';
 import clickElement from '../support/action/clickElement';
@@ -19,89 +19,87 @@ import setPromptText from '../support/action/setPromptText';
 import submitForm from '../support/action/submitForm';
 
 
-defineSupportCode(({ When }) => {
-    When(
-        /^I (click|doubleclick) on the (link|button|element) "([^"]*)?"$/,
-        clickElement
-    );
+When(
+    /^I (click|doubleclick) on the (link|button|element) "([^"]*)?"$/,
+    clickElement
+);
 
-    When(
-        /^I (add|set) "([^"]*)?" to the inputfield "([^"]*)?"$/,
-        setInputField
-    );
+When(
+    /^I (add|set) "([^"]*)?" to the inputfield "([^"]*)?"$/,
+    setInputField
+);
 
-    When(
-        /^I clear the inputfield "([^"]*)?"$/,
-        clearInputField
-    );
+When(
+    /^I clear the inputfield "([^"]*)?"$/,
+    clearInputField
+);
 
-    When(
-        /^I drag element "([^"]*)?" to element "([^"]*)?"$/,
-        dragElement
-    );
+When(
+    /^I drag element "([^"]*)?" to element "([^"]*)?"$/,
+    dragElement
+);
 
-    When(
-        /^I submit the form "([^"]*)?"$/,
-        submitForm
-    );
+When(
+    /^I submit the form "([^"]*)?"$/,
+    submitForm
+);
 
-    When(
-        /^I pause for (\d+)ms$/,
-        pause
-    );
+When(
+    /^I pause for (\d+)ms$/,
+    pause
+);
 
-    When(
-        /^I set a cookie "([^"]*)?" with the content "([^"]*)?"$/,
-        setCookie
-    );
+When(
+    /^I set a cookie "([^"]*)?" with the content "([^"]*)?"$/,
+    setCookie
+);
 
-    When(
-        /^I delete the cookie "([^"]*)?"$/,
-        deleteCookie
-    );
+When(
+    /^I delete the cookie "([^"]*)?"$/,
+    deleteCookie
+);
 
-    When(
-        /^I press "([^"]*)?"$/,
-        pressButton
-    );
+When(
+    /^I press "([^"]*)?"$/,
+    pressButton
+);
 
-    When(
-        /^I (accept|dismiss) the (alertbox|confirmbox|prompt)$/,
-        handleModal
-    );
+When(
+    /^I (accept|dismiss) the (alertbox|confirmbox|prompt)$/,
+    handleModal
+);
 
-    When(
-        /^I enter "([^"]*)?" into the prompt$/,
-        setPromptText
-    );
+When(
+    /^I enter "([^"]*)?" into the prompt$/,
+    setPromptText
+);
 
-    When(
-        /^I scroll to element "([^"]*)?"$/,
-        scroll
-    );
+When(
+    /^I scroll to element "([^"]*)?"$/,
+    scroll
+);
 
-    When(
-        /^I close the last opened (window|tab)$/,
-        closeLastOpenedWindow
-    );
+When(
+    /^I close the last opened (window|tab)$/,
+    closeLastOpenedWindow
+);
 
-    When(
-        /^I focus the last opened (window|tab)$/,
-        focusLastOpenedWindow
-    );
+When(
+    /^I focus the last opened (window|tab)$/,
+    focusLastOpenedWindow
+);
 
-    When(
-        /^I select the (\d+)(st|nd|rd|th) option for element "([^"]*)?"$/,
-        selectOptionByIndex
-    );
+When(
+    /^I select the (\d+)(st|nd|rd|th) option for element "([^"]*)?"$/,
+    selectOptionByIndex
+);
 
-    When(
-        /^I select the option with the (name|value|text) "([^"]*)?" for element "([^"]*)?"$/,
-        selectOption
-    );
+When(
+    /^I select the option with the (name|value|text) "([^"]*)?" for element "([^"]*)?"$/,
+    selectOption
+);
 
-    When(
-        /^I move to element "([^"]*)?"(?: with an offset of (\d+),(\d+))*$/,
-        moveToElement
-    );
-});
+When(
+    /^I move to element "([^"]*)?"(?: with an offset of (\d+),(\d+))*$/,
+    moveToElement
+);
