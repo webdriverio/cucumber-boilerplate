41770de69808af47ec326e0aba808b51596e7d96
Lewis Hadden <lewishadden@gmail.com>
2021-04-22 11:40:02 +0100

    Fixes closeLastOpenedWindow

--- test/support/action/closeLastOpenedWindow.spec.js
+++ test/support/action/closeLastOpenedWindow.spec.js
@@ -8,20 +8,47 @@ describe('closeLastOpenedWindow', () => {
                 'two',
                 'three',
             ]),
+            getWindowHandle: jest.fn(() => 'three'),
             switchToWindow: jest.fn(),
             closeWindow: jest.fn(),
         };
     });
 
-    it('should call closeLastOpenedWindow on the browser', () => {
-        closeLastOpenedWindow('');
+    describe('when focused on the last opened window', () => {
+        it('should call closeLastOpenedWindow on the browser', () => {
+            closeLastOpenedWindow('');
 
-        expect(global.browser.getWindowHandles).toHaveBeenCalledTimes(1);
+            expect(global.browser.getWindowHandles).toHaveBeenCalledTimes(1);
 
-        expect(global.browser.switchToWindow).toHaveBeenCalledTimes(1);
+            expect(global.browser.switchToWindow).toHaveBeenCalledTimes(0);
 
-        expect(global.browser.switchToWindow).toHaveBeenCalledWith('three');
+            expect(global.browser.closeWindow).toHaveBeenCalledTimes(1);
+        });
+    });
 
-        expect(global.browser.closeWindow).toHaveBeenCalledTimes(1);
+    describe('when NOT focused on the last opened window', () => {
+        beforeEach(() => {
+            global.browser.getWindowHandle = jest.fn(() => 'two');
+        });
+
+        it('should switch to the last opened window', () => {
+            closeLastOpenedWindow('');
+
+            expect(global.browser.switchToWindow).toHaveBeenCalledTimes(2);
+
+            expect(global.browser.switchToWindow).toHaveBeenCalledWith('three');
+
+            expect(global.browser.closeWindow).toHaveBeenCalledTimes(1);
+        });
+
+        it('should switch back to the last focused window', () => {
+            closeLastOpenedWindow('');
+
+            expect(global.browser.switchToWindow).toHaveBeenCalledTimes(2);
+
+            expect(global.browser.switchToWindow).toHaveBeenCalledWith('two');
+
+            expect(global.browser.closeWindow).toHaveBeenCalledTimes(1);
+        });
     });
 });
